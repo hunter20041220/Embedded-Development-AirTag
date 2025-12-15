e5fdf41c92d7ccfc02c0da24b2994366c75f4bba
hunter20041220 <3123001378@mail2.gdut.edu.cn>
2025-12-15 14:28:26 +0800

    feature(test):test oled

--- ESP32_Distance_Tracker/esp-idf_hello_world/hello_world/build/CMakeFiles/git-data/grabRef.cmake
+++ ESP32_Distance_Tracker/esp-idf_hello_world/hello_world/build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "D:/progress/code/Embedded-Development-AirTag/ESP32_Distance_Tracker/esp-idf_hello_world/hello_world/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "D:/progress/esp/esp-idf/v5.5.1/esp-idf/.git")
+set(GIT_DIR "D:/progress/code/Embedded-Development-AirTag/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
